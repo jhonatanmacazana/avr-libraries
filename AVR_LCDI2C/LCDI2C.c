3cb50fe94fe61959c07e390717ed70f74b056bf9
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2019-01-26 00:45:59 -0500

    LCD-I2Cadapter Library

---
+++ AVR_LCDI2C/LCDI2C.c
@@ -0,0 +1,227 @@
+/*************************************************************************
+ Title	:   LCD - I2C adapter library (LCDI2C.c)
+ Author:    Jhonatan Macazana <jhonatan.macazana@utec.edu.pe> 
+ Software:  AVR-GCC 4.x
+ Hardware:  Designed for ATmega328P, similar AVR devices
+
+ DESCRIPTION
+       Basic routines for the I2C protocol and LCD commands.
+
+       Originally based on the Github Code of eagl1 
+       (https://github.com/eagl1/LCD1602_I2C_all_code). Changed
+       the macros and new functions for the LCD. 
+       Designed for the ATmega328P microcontroller.
+
+       This Library only uses the I2C pins of the AVR. 
+       The bit rate to initialize the I2C is MYTWBR. 
+       This value is obtain from a Macro definition.
+
+ USAGE
+       See the C include LCDI2C.h file for a description of each function
+       
+*****************************************************************************/
+
+#include <avr/io.h>
+#include <util/twi.h>
+#include <stdlib.h>
+#include "LCDI2C.h"
+
+
+/*
+**	functions - LCD
+*/
+
+
+/*************************************************************************
+Low-level function to initialize the LCD.
+Input:    none
+Returns:  none
+*************************************************************************/
+void LCD_Init(void)
+{
+	/* Initialize LCD */
+	sendCMD(function_set1);
+	sendCMD(function_set2);
+	sendCMD(LCD_FUNCTION_4BIT_2LINES);
+	sendCMD(LCD_DISP_ON);
+	sendCMD(LCD_CLR);
+}
+
+/*************************************************************************
+Allows to write new commands to the LCD 
+Input:    CMD	Command to be send
+Returns:  none
+*************************************************************************/
+void sendCMD(uint8_t CMD)
+{
+	uint8_t bitmask;
+	/* Send Address - Write Condition */
+	I2C_Start(LCD_Add_WR);
+	
+	/* Send commands. MS Nibble*/
+	I2C_Transmit(bitmask = (CMD & 0xF0) | 0x08);
+	I2C_Transmit(bitmask |= (1 << E));
+	I2C_Transmit(bitmask &= ~(1 << E));
+	
+	/* Send commands. LS Nibble*/
+	I2C_Transmit(bitmask = ((CMD << 4) & 0xF0) | 0x08);
+	I2C_Transmit(bitmask |= (1 << E));
+	I2C_Transmit(bitmask &= ~(1 << E));
+	
+	/* Stop Condition */
+	I2C_Stop();
+}
+
+/*************************************************************************
+Put a char on the LCD Display. 
+Input:    data	char to be shown
+Returns:  none
+*************************************************************************/
+void sendData(uint8_t data)
+{
+	uint8_t bitmask;
+	/* Send Address - Write Condition */
+	I2C_Start(LCD_Add_WR);
+	
+	/* Send Data. MS Nibble*/
+	I2C_Transmit(bitmask = (data & 0xF0) | 0x08 | (1 << RS));
+	I2C_Transmit(bitmask |= (1 << E));
+	I2C_Transmit(bitmask &= ~(1 << E));
+	
+	/* Send Data. LS Nibble*/
+	I2C_Transmit(bitmask = ((data << 4) & 0xF0) | 0x08 | (1 << RS));
+	I2C_Transmit(bitmask |= (1 << E));
+	I2C_Transmit(bitmask &= ~(1 << E));
+	
+	/* Stop Condition */
+	I2C_Stop();
+}
+
+/*************************************************************************
+Change the current position of the cursor 
+Input:    row	Choose the new row
+		  col 	Choose the new col
+Returns:  none
+*************************************************************************/
+void LCD_GotoXY (uint8_t row, uint8_t col)
+{
+	/* Change the cursor*/
+	if (row == 1)
+	sendCMD(LCD_ROW1 + col);
+	if (row == 2)
+	sendCMD(LCD_ROW2 + col);
+}
+
+/*************************************************************************
+Put a String on the LCD Display. 
+Input:    arr1	String to be shown
+Returns:  none
+*************************************************************************/
+void LCD_String(char* arr1)
+{
+	/* Last char will be null. Check for characters to send*/
+	while(*arr1 != 0x00)
+	{
+		sendData(*arr1);		// Send 1 char at the time
+		arr1++;					// Increment the index
+	}
+}
+
+/*************************************************************************
+Put a Number on the LCD Display. 
+Input:    numb	number to be shown
+Returns:  none
+*************************************************************************/
+void LCD_Number(uint16_t numb)
+{
+	char array[4];				// 4 digits of the number. Change if needed
+	itoa(numb, array, 10);		// Radix for the conversion: 10
+	LCD_String(array);			// Send the ASCII codes obtained from data
+	
+}
+
+
+/*
+**	functions - I2C
+*/
+
+/*************************************************************************
+Low-level function to initialize the I2C
+Input:    none
+Returns:  none
+*************************************************************************/
+void I2C_Init(void)
+{
+	/* Set SCL to 50 kHz. Macro definition */
+	TWSR = 0x00;			// Prescaler = 1
+	TWBR = MYTWBR;			// Macro 72
+	
+	/* Enable TWI */
+	TWCR = (1 << TWEN);
+}
+
+
+/*************************************************************************
+Low-level function that send a Start condition and the address of a device
+Input:    address 	Address of device with a W/R condition at the end.
+Returns:  none
+*************************************************************************/
+void I2C_Start(uint8_t address)
+{
+	/* Send Start Condition*/
+	TWCR |= (1 << TWINT) | (1 << TWSTA);
+	
+	/* Wait for the acknowledge bit */
+	while (!(TWCR & (1 << TWINT)));
+	
+	/* Send the address and W/R condition */
+	TWDR = address;
+	TWCR |= (1 << TWINT) | (1 << TWEA);
+	
+	/* Wait for the acknowledge bit */
+	while (!(TWCR & (1 << TWINT)));
+}
+
+/*************************************************************************
+Low-level function that send a Stop condition.
+Input:    none
+Returns:  none
+*************************************************************************/
+void I2C_Stop(void)
+{
+	/* Send Stop Condition */
+	TWCR |= (1 << TWINT) | (1 << TWSTO);
+}
+
+
+/*************************************************************************
+Send Byte through I2C. Wait for the acknowledge bit.
+Input:    data 	byte to be send
+Returns:  none
+*************************************************************************/
+void I2C_Transmit(uint8_t data)
+{
+	/* Send the Data */
+	TWDR = data;
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+	
+	/* Wait for the acknowledge bit */
+	while (!(TWCR & (1 << TWINT)));
+}
+
+/*************************************************************************
+Waits until there are new data. 
+Input:    none
+Returns:  Data received from the UART. 
+*************************************************************************/
+uint8_t I2C_Receive(void)
+{
+	/*Clean the flag for the incoming data*/
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+	
+	/* Wait for the acknowledge bit */
+	while (!(TWCR & (1 << TWINT)));
+	
+	/* Return the incoming data */
+	return TWDR;
+}
---
+++ AVR_LCDI2C/LCDI2C.h
@@ -0,0 +1,177 @@
+#ifndef LCDI2C_H_
+#define LCDI2C_H_
+
+/*************************************************************************
+ Title	:   C include file for the LCD - I2C adapter library (LCDI2C.c)
+ Author	:   Jhonatan Macazana <jhonatan.macazana@utec.edu.pe> 
+ Software:  AVR-GCC 4.x
+ Hardware:  Designed for ATmega328P, similar AVR devices
+
+ DESCRIPTION
+       Basic routines for the I2C protocol and LCD commands.
+
+       Originally based on the Github Code of eagl1 
+       (https://github.com/eagl1/LCD1602_I2C_all_code). Changed
+       the macros and new functions for the LCD. 
+       Designed for the ATmega328P microcontroller.
+
+       This Library only uses the I2C pins of the AVR. 
+       The bit rate to initialize the I2C is MYTWBR. 
+       This value is obtain from a Macro definition.
+
+*****************************************************************************/
+
+
+/**
+*	I2C Clock Definitions
+*	Used to obtain the UBRR value for the desired baud rate.
+*
+*/
+#ifndef F_CPU		/* Prevent compiler error by supplying a default */
+#define F_CPU 1000000UL
+#endif
+#define I2C_VEL			10000			// 10 kHz for the I2C
+#define MYTWBR			((F_CPU/I2C_VEL - 16)/2)	
+
+
+/**
+*	I2C-Adapter Address
+*	Choose the address of the I2C adapter. The default values 
+*	are 0x27 and 0x37. Change if needed.
+*
+*/
+#define LCD_Add			0x27			// Address of the LCD
+
+
+/**
+*	LCD Pins definition
+*	Choose the enable, reset and read/write pins of the LCD. The default 
+* 	values are 2,1,0 respectively. 
+*
+*/
+#define E				2   			// E  bit
+#define RW				1   			// RW bit
+#define RS				0   			// RS bit
+
+
+/**
+*	LCD Command Definitions
+*	Used to configure the device. This commands are used on the 
+*	LCD_Init() function. 
+*
+*/
+#define function_set1				0x33
+#define function_set2				0x32
+#define LCD_CLR						0x01
+#define LCD_ROW1					0x80
+#define LCD_ROW2					0xC0
+#define LCD_DISP_ON					0x0C
+#define LCD_DISP_ON_CURSOR			0x0E
+#define LCD_DISP_ON_CURSOR_BLINK	0x0F
+#define LCD_FUNCTION_4BIT_2LINES	0x28
+
+
+/**
+*	LCD Macro Definitions
+*	A brief set of macros to make the code easier to read.
+*
+*/
+#define LCD_Add_WR		((LCD_Add<<1)|TW_WRITE)
+#define LCD_Add_RD		((LCD_Add<<1)|TW_READ)
+#define LCD_Clear()		(sendCMD(LCD_CLR))
+#define LCD_Up()		(sendCMD(LCD_ROW1))
+#define LCD_Down()		(sendCMD(LCD_ROW2))
+
+
+
+/**
+*	Functions - LCD
+*/
+
+/**
+ @brief		Low-level function to initialize the LCD. 
+ @param		none
+ @return 	none
+*/
+void LCD_Init(void);
+
+/**
+ @brief		Allows to write new commands to the LCD.
+ @param		CMD 	Command to be send
+ @return 	none
+*/
+void sendCMD(uint8_t CMD);
+
+/**
+ @brief		Change the current position of the cursor 
+ @param		row 	Choose the new row
+ 			col 	Choose the new col
+ @return 	none
+*/
+void LCD_GotoXY (uint8_t row, uint8_t col);
+
+/**
+ @brief		Put a String on the LCD Display. 
+ @param		arr1	String to be shown
+ @return 	none
+*/
+void LCD_String(char* arr1);
+
+/**
+ @brief		Put a Number on the LCD Display.
+ @param		numb	number to be shown
+ @return 	none
+*/
+void LCD_Number(uint16_t numb);
+
+
+
+
+/**
+*	Functions - I2C
+*/
+
+/**
+ @brief		Low-level function to initialize the I2C
+ @param		none
+ @return 	none
+*/
+void I2C_Init(void);
+
+/**
+ @brief		Low-level function that send a Start condition and the address of a device.
+ @param		address 	Address of device with a W/R condition at the end.
+ @return 	none
+*/
+void I2C_Start(uint8_t address);
+
+/**
+ @brief		Low-level function that send a Stop condition.
+ @param		none
+ @return 	none
+*/
+void I2C_Stop(void);
+
+/**
+ @brief		Send Byte through I2C. Wait for the acknowledge bit.
+ @param		data 	byte to be send
+ @return 	none
+*/
+void I2C_Transmit(uint8_t data);
+
+/**
+ @brief		Waits until there are new data. 
+ @param		none
+ @return 	Data received from the UART. 
+*/
+uint8_t I2C_Receive(void);
+
+/**
+ @brief		Low-level function to initialize the LCD. 
+ @param		none
+ @return 	none
+*/
+
+
+
+#endif /* LCDI2C_H_ */
